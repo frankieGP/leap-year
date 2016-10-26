feda9798412688ff2403f920fdd59f7a218a4cba
Frankie Gavin and Quinn Schwartz <frankie@>
2016-10-26 10:16:46 -0700

    add additional user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,21 @@ $(document).ready(function(){
     event.preventDefault();
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-    $("#result").text(result);
+
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
+
+
 // Business Logic
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
